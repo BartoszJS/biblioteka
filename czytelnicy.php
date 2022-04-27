5465e91f32f4eae649a8d6fff2b7c74f7c680eb0
BartoszJS <bartosz290300@wp.pl>
2022-04-27 23:55:53 +0200

    8 30%

--- czytelnicy.php
+++ czytelnicy.php
@@ -3,11 +3,75 @@ include 'src/bootstrap.php';
 include 'src/database-connection.php'; 
 include 'src/validate.php';
 
+$term  = filter_input(INPUT_GET, 'term');                 // Get search term
+$show  = filter_input(INPUT_GET, 'show', FILTER_VALIDATE_INT) ?? 10; // Limit
+$from  = filter_input(INPUT_GET, 'from', FILTER_VALIDATE_INT) ?? 0; // Offset
+$count = 0;
+$czytelnicy=[];
 
-$sql="SELECT ID,Imie,Nazwisko,Numer_Telefonu,Adres_Email
-    FROM czytelnik
-    order by id desc;";
-$czytelnicy = pdo($pdo,$sql)->fetchAll();
+
+if(!$term){
+    $count = 0;
+    $sqlicz="SELECT COUNT(id) from czytelnik ;";
+    $count = pdo($pdo, $sqlicz)->fetchColumn();
+    if($count>0){
+        $arguments['show'] = $show;                     
+        $arguments['from'] = $from;
+
+        $sql="SELECT ID,imie,nazwisko,numer_telefonu,adres_email
+        FROM czytelnik  
+        order by id desc
+        limit :show
+        OFFSET :from;";
+        $czytelnicy = pdo($pdo,$sql, $arguments)->fetchAll();
+    }
+}
+
+
+
+
+
+
+if($term){
+    
+    $arguments['term1'] ='%'.$term.'%'; 
+    $arguments['term2'] ='%'.$term.'%';            // three times as placeholders
+    $arguments['term3'] ='%'.$term.'%';
+
+
+    $sql="SELECT COUNT(id) 
+    from czytelnik
+    where imie like :term1
+    or id like :term2
+    or nazwisko like :term3;";
+
+
+    $count = 0;
+    $count = pdo($pdo, $sql, $arguments)->fetchColumn();
+
+    if ($count > 0) {  
+        $arguments['show'] = $show;                       // Add to array for pagination
+        $arguments['from'] = $from; 
+        
+
+        $sql="SELECT ID,imie,nazwisko,numer_telefonu,adres_email
+            FROM czytelnik
+            where imie like :term1
+            or id like :term2
+             or nazwisko like :term3
+            order by id desc
+            limit :show
+            OFFSET :from;";
+        $czytelnicy = pdo($pdo,$sql,$arguments)->fetchAll();
+    }
+}
+
+
+if ($count > $show) {                                     // If matches is more than show
+    $total_pages  = ceil($count / $show);                 // Calculate total pages
+    $current_page = ceil($from / $show) + 1;              // Calculate current page
+}
+
 
 
 ?>
@@ -25,14 +89,13 @@ $czytelnicy = pdo($pdo,$sql)->fetchAll();
 
 </head>
 <body>
-<br><br>
 <div class="czytelnicy">
 
     <div class="ramka1">
     <h1>Czytelnicy: </h1>
     <br>
     <div class="szukaj">
-    <form action="oferty.php" method="get" class="form-search">
+    <form action="czytelnicy.php" method="get" class="form-search">
                 <label for="search"><span> </span></label>
                 <input type="text" name="term" 
                     id="search" placeholder="Wyszukaj tutaj:"  
@@ -50,10 +113,10 @@ $czytelnicy = pdo($pdo,$sql)->fetchAll();
                 <div class="tekst">
                     <div class="id">  <?= $pojedynczo['ID']; ?></div>
                 
-                    <?= $pojedynczo['Imie']; ?>
-                    <?= $pojedynczo['Nazwisko']; ?><br>
-                    <?= "Nr telefonu: ".$pojedynczo['Numer_Telefonu']; ?> <br>
-                    <?= "E-mail: ".$pojedynczo['Adres_Email']; ?>
+                    <?= $pojedynczo['imie']; ?>
+                    <?= $pojedynczo['nazwisko']; ?><br>
+                    <?= "Nr telefonu: ".$pojedynczo['numer_telefonu']; ?> <br>
+                    <?= "E-mail: ".$pojedynczo['adres_email']; ?>
                 </div>
                 <div class="przyciski">
                 <a href="czytelnik.php" class="btnzobacz">ZOBACZ</a> 
